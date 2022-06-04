3b8827af1fa7f3abc5e036b51a83908ac4782d5f
Cem Şahin <cmshn02@gmail.com>
2022-06-04 11:37:37 +0300

    Add project files.

---
+++ TravelTrip/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+
+namespace TravelTrip.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+
+            var degeler = c.Hakkimizdas.ToList();
+
+            return View(degeler);
+        }
+    }
+}
---
+++ TravelTrip/Controllers/DenemeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+
+namespace TravelTrip.Controllers
+{
+    public class DenemeController : Controller
+    {
+        Context c = new Context();
+        // GET: Deneme
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            
+            return View(degerler);
+        }
+
+
+       
+        public ActionResult About()
+        {
+
+
+
+            return View();
+        }
+
+        public PartialViewResult Partial1()
+        {
+
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+
+        public PartialViewResult Partial2()
+        {
+
+            var degerler = c.Blogs.Where(x => x.ID==1).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial3()
+        {
+
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(10).ToList();
+            return PartialView(degerler);
+        }
+
+
+
+        public PartialViewResult Partial4()
+        {
+
+            var degerler = c.Blogs.Take(3).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial5()
+        {
+
+            var degerler = c.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList();
+            return PartialView(degerler);
+        }
+    }
+}
